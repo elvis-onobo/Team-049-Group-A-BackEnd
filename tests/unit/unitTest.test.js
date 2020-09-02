2227fdbadc92f4cc002355855c319c10b0e47697
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-09-02 21:32:59 +0100

    Chore(Test): Add test scaffold

---
+++ tests/unit/unitTest.test.js
@@ -0,0 +1,11 @@
+/**
+ * This is just a simple test to ensure the scaffold works
+ */
+
+function sum(a, b) {
+  return a + b;
+}
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
