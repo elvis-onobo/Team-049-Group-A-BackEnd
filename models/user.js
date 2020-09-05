b0ca576d1d09f46c11c16660b2c8ef3e3b70f117
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-09-05 23:22:48 +0100

    Chore: Add registartion route

--- models/user.js
+++ models/user.js
@@ -3,21 +3,23 @@
 const { Model } = require("objection");
 
 class User extends Model {
+  // bind your model to the corresponsing migration
   static get tableName() {
     return "users";
   }
 
-  // Create JSON validation schema
+  // Add validation rules for your model
   static get jsonSchema() {
     return {
       type: "object",
-      required: ["firstname", "lastname", "phone", "password"],
+      required: ["firstname", "lastname", "phone", "email", "password"],
 
       properties: {
         id: { type: "integer" },
         firstname: { type: "string", minLength: 1, maxLength: 255 },
         lastname: { type: "string", minLength: 1, maxLength: 255 },
         phone: { type: "string", minLength: 11, maxLength: 11 },
+        email: { type: "string", minLength: 3, maxLength: 255 },
         password: { type: "string", minLength: 6, maxLength: 255 },
       },
     };
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ const express = require("express");
 const router = new express.Router();
 
 const baseController = require("../controllers/baseController");
+const RegisterController = require("../controllers/auth/RegisterController");
 
 router.get("/", baseController.renderData);
+router.post("/user/register", RegisterController.registerUser);
 
 // not found route
 router.use((req, res, next) => {
