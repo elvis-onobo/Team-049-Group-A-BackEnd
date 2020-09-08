66b988ff36d06dfb7e713f9e6a064e46687f5c6f
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-09-08 06:30:47 +0100

    Feat(Registration): Add email verification

---
+++ controllers/auth/VerifyEmail.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const User = require("../../models/User");
+
+exports.verifyEmail = async (req, res) => {
+  const { key } = req.params;
+
+  try {
+    const user = await User.query().patch({ verified: 1 }).where("key", key);
+
+    res.status(200).json({ status: "success", message: "Email verified" });
+  } catch (verifyEmailError) {
+    console.log("Error verifying email", verifyEmailError);
+
+    res.json({ status: "failed", message: verifyEmailError.message });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,11 @@ const router = new express.Router();
 
 const baseController = require("../controllers/baseController");
 const RegisterController = require("../controllers/auth/RegisterController");
+const VerifyEmail = require("../controllers/auth/VerifyEmail");
 
 router.get("/", baseController.renderData);
 router.post("/user/register", RegisterController.registerUser);
+router.put("/user/verify/email/:key", VerifyEmail.verifyEmail);
 
 // not found route
 router.use((req, res, next) => {
