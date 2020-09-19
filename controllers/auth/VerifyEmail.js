7e3762537e5b1aabe6867416295136ef15bf1a96
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-09-19 21:31:42 +0100

    Fix(Auth): fix verify email to update the key on verification

--- controllers/auth/VerifyEmail.js
+++ controllers/auth/VerifyEmail.js
@@ -1,12 +1,21 @@
 "use strict";
 
+const randomstring = require("randomstring");
+
 const User = require("../../models/User");
 
 exports.verifyEmail = async (req, res) => {
   const { key } = req.params;
 
+  const newKey = randomstring.generate({
+    length: 10,
+    charset: "alphanumeric",
+  });
+
   try {
-    const user = await User.query().patch({ verified: 1 }).where("key", key);
+    const user = await User.query()
+      .patch({ verified: 1, key: newKey })
+      .where("key", key);
 
     res.status(200).json({ status: "success", message: "Email verified" });
   } catch (verifyEmailError) {
